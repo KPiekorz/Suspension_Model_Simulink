c8f4698c830d831652cb90110a3a25f565d19953
Krzysztof <kpiekorz919@protonmail.com>
2021-07-25 22:31:50 +0200

    only force

--- parameters.m
+++ parameters.m
@@ -5,12 +5,12 @@ T = 50;
 step = 0.01; 
 
 
-M1  = 2500; % kg - 1/4 car
-M2 = 320; % kg - suspension mass
+M1  = 350; % kg - 1/4 car
+M2 = 50; % kg - suspension mass
 
 
-k1 = 80000;
-k2 = 500000;
+k1 = 8000;
+k2 = 50000;
 
-b1 = 350;
-b2 = 15020;
+b1 = 35;
+b2 = 1500;
